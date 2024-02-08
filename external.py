53e5ba773a1a5a1673277dc979975a3e58b69ddc
pallavi <pallavikocherla16046@gmail.com>
2024-02-08 11:08:37 +0530

    sample files

---
+++ external.py
@@ -0,0 +1,11 @@
+print("To print 10 multiples of a number.")
+print("Enter the number: ", end="")
+num = int(input())
+print("The first 10 multiples of ", num, " are ", end="", sep="")
+for i in range(1, 11):  
+    i <= 10
+    count = num * i
+    print(count, ", ", end="", sep="")
+print(count, ".", end="", sep="")
+
+    
